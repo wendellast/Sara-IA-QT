4e926404b7c0e9d281cd13bf1c1a6683f8918a3e
wendellast <wendelalves1161@gmail.com>
2022-06-07 10:38:04 -0300

    Melhoria na função cadastro de banco de dados

--- test/test.py
+++ test/test.py
@@ -2,14 +2,37 @@ import sqlite3
 
 
 def cadastrar(): # Pronto // Precisa melhorar
-    nome = str(input('Digite o seu nome: '))
-    senha = str(input('Digite a sua senha: '))
-    c_senha = str(input('confirme a sua senha'))
+  
+    while True:
+
+        try:
+            nome = str(input('Digite o seu nome: '))
+            senha = str(input('Digite a sua senha: '))
+            c_senha = str(input('confirme a sua senha'))
+
+            banco = sqlite3.connect('banco_cadastro.db') 
+
+            cursor = banco.cursor()
+            cursor.execute(f'SELECT nome FROM cadastro  WHERE nome="{nome}"')
+            nome_db = cursor.fetchall()
+
+            cursor.execute(f'SELECT senha FROM cadastro  WHERE nome="{nome}"')
+            senha_db = cursor.fetchall()
+            
+        except:
+            print('Erro de acesso')
+
+        try:
+            if  senha ==  senha_db[0][0] and nome ==  nome_db[0][0]:
+                print('Usuario já cadastrado')
+                print('Tente novamente')
+                continue
+
+        except:
+            break
 
     if (senha == c_senha):
         try:
-            banco = sqlite3.connect('banco_cadastro.db') 
-            cursor = banco.cursor()
             cursor.execute("CREATE TABLE IF NOT EXISTS cadastro (nome text, senha text)")
             cursor.execute(f"INSERT INTO cadastro VALUES('{nome}', '{senha}') ")
 
@@ -39,13 +62,15 @@ def login():
         banco.close()
 
     except:
-        print('Usuario não cadastrado')
+        print('Erro de conexão')
 
     try:
         if  senha ==  senha_db[0][0] and nome_usuario ==  nome_db[0][0]:
             print('logado')
-        else:
-            print('Erro senha incorreta')
+
     except:
-        print('Erro usuario não encontrado')
+        print('Usuario e senha incorretos')
+
+
+
 login()
